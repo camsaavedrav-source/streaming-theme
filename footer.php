4f3094dfada600cc3864d81bbff8b08fdeb9e135
Camila Saavedra <cam.saavedrav@duocuc.cl>
2026-06-22 23:13:06 -0400

    cambiar header y footer

--- footer.php
+++ footer.php
@@ -11,21 +11,8 @@
 
 ?>
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'plataforma' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'plataforma' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'plataforma' ), 'plataforma', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+		<?php include get_template_directory() . '/assets/templates/footers/template-footer.php'; ?>
+
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
